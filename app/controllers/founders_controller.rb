d8768d83562717fb2e89a252084f3e872518f9dd
David Ladowitz <david@ladowitz.com>
2015-07-08 20:57:41 -0700

    removing founders controller for starter reference 3

--- app/controllers/founders_controller.rb
+++
@@ -1,5 +0,0 @@
-class FoundersController < ApplicationController
-  def index
-    render layout: "guest_pages/guest_layout"
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,6 @@ Rails.application.routes.draw do
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
   resources :password_resets, only: [:create]
-  resources :founders, only: [:index]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
