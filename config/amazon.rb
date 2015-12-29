9dde37d76aebc1853a83f9dc6b82dc4b05b85a89
David Ladowitz <david@ladowitz.com>
2015-12-29 13:27:40 -0800

    updating prices

---
+++ config/amazon.rb
@@ -0,0 +1,23 @@
+# Subscription Packages
+
+# Two Guys In a Garage
+# Promised: 4 markers / $6
+# Sending: Extra eraser and spray
+# http://www.amazon.com/Expo-Original-Chisel-6-Piece-Assorted/dp/B00006IFIV/ref=sr_1_13?s=office-products&ie=UTF8&qid=1451422150&sr=1-13&keywords=dry+erase+markers&refinements=p_85%3A2470955011%2Cp_n_location_browse-bin%3A6473192011%2Cp_n_is-min-purchase-required%3A5016683011
+
+# Seed Money
+# Promised: 8 markers / $7
+# Sending: 8 Markers
+# http://www.amazon.com/AmazonBasics-Low-Odor-Dry-Erase-Markers/dp/B00T3ROUES/ref=sr_1_26?ie=UTF8&qid=1451422835&sr=8-26&keywords=dry+erase+markers+amazon+chisel
+
+# Venture Capital
+# Promised: 16 markers / 1 Eraser $14
+# Sending: 16 markers / 1 Eraser
+# 2 x http://www.amazon.com/AmazonBasics-Low-Odor-Dry-Erase-Markers/dp/B00T3ROUES/ref=sr_1_26?ie=UTF8&qid=1451422835&sr=8-26&keywords=dry+erase+markers+amazon+chisel
+# 1 x http://www.amazon.com/Expo-81505-Whiteboard-Eraser-8-inch/dp/B000SHQ73Y/ref=sr_1_4?s=office-products&ie=UTF8&qid=1451424068&sr=1-4&keywords=dry+erase+eraser&refinements=p_85%3A2470955011
+
+# IPO
+# Promised: 36 markers / 3 Ersasers / $28
+# Sending: 36 markers / 3 Ersasers
+# 3 x http://www.amazon.com/AmazonBasics-Low-Odor-Dry-Erase-Markers/dp/B00T3ROUES/ref=sr_1_26?ie=UTF8&qid=1451422835&sr=8-26&keywords=dry+erase+markers+amazon+chisel
+# 3 x http://www.amazon.com/Expo-81505-Whiteboard-Eraser-8-inch/dp/B000SHQ73Y/ref=sr_1_4?s=office-products&ie=UTF8&qid=1451424068&sr=1-4&keywords=dry+erase+eraser&refinements=p_85%3A2470955011
